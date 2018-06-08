0d5896a2c16aa1a264a62bf271c4daf3a9e3dbfd
Adrian Jimenez <adrianx30@gmail.com>
2018-06-08 11:38:37 -0500

    little refactor login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,19 +7,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  /* private email: FormControl;
-  private password: FormControl; */
   private loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
-    /* this.email = new FormControl('', [Validators.required, Validators.email]);
-    this.password = new FormControl('', [Validators.required, Validators.minLength(6)]);
-    this.loginForm = new FormGroup({
-      email: this.email,
-      password: this.password
-    }); */
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])

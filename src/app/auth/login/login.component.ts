b19853efc96bd7ec95f78216a9a675de1ef6a9b5
Adrian Jimenez <adrianx30@gmail.com>
2018-06-07 19:29:12 -0500

    login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  /* private email: FormControl;
+  private password: FormControl; */
+  private loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    /* this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+    this.loginForm = new FormGroup({
+      email: this.email,
+      password: this.password
+    }); */
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
 }

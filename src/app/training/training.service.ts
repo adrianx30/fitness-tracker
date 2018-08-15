b46a0dcc4432e725d1bfc32a79fea245b99fecac
Adrian Jimenez <adrianx30@gmail.com>
2018-08-14 19:20:58 -0500

    storing data in firebase

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,10 +8,10 @@ import { AngularFirestore, DocumentChangeAction } from 'angularfire2/firestore';
 export class TrainingService {
   exerciseChange = new Subject<Exercise>();
   exercisesChanged = new Subject<Exercise[]>();
-  private availableExercises: Exercise[] = [];
+  finishedExercisesChanged = new Subject<Exercise[]>();
 
+  private availableExercises: Exercise[] = [];
   private runningExercise: Exercise;
-  private exercises: Exercise[] = [];
 
   constructor(private db: AngularFirestore) {}
 
@@ -19,31 +19,35 @@ export class TrainingService {
     this.db
       .collection('availableExercises')
       .snapshotChanges()
-      .pipe(map(docArray => {
-        return docArray.map((doc: DocumentChangeAction<Exercise>) => {
-          return {
-            id: doc.payload.doc.id,
-            // name: doc.payload.doc.data().name,
-            // duration: doc.payload.doc.data().duration,
-            // calories: doc.payload.doc.data().calories
-            ...doc.payload.doc.data()
-          };
-        });
-      }))
+      .pipe(
+        map(docArray => {
+          return docArray.map((doc: DocumentChangeAction<Exercise>) => {
+            return {
+              id: doc.payload.doc.id,
+              // name: doc.payload.doc.data().name,
+              // duration: doc.payload.doc.data().duration,
+              // calories: doc.payload.doc.data().calories
+              ...doc.payload.doc.data()
+            };
+          });
+        })
+      )
       // .valueChanges(); // gets only the data of the document
-      .subscribe((exercises) => {
+      .subscribe(exercises => {
         this.availableExercises = exercises;
         this.exercisesChanged.next([...this.availableExercises]);
       });
   }
 
   startExercise(selectedId: string) {
-    this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
-    this.exerciseChange.next({ ...this.runningExercise});
+    this.runningExercise = this.availableExercises.find(
+      ex => ex.id === selectedId
+    );
+    this.exerciseChange.next({ ...this.runningExercise });
   }
 
   completeExercise() {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       date: new Date(),
       state: 'completed'
@@ -53,7 +57,7 @@ export class TrainingService {
   }
 
   cancelExercise(progress: number) {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       duration: this.runningExercise.duration * (progress / 100),
       calories: this.runningExercise.calories * (progress / 100),
@@ -65,11 +69,19 @@ export class TrainingService {
   }
 
   getRunningExercise(): Exercise {
-    return { ...this.runningExercise};
+    return { ...this.runningExercise };
   }
 
-  getCompletedOrCancelledExercises() {
-    return this.exercises.slice();
+  fetchCompletedOrCancelledExercises() {
+    this.db
+      .collection('finishedExercises')
+      .valueChanges()
+      .subscribe((exercises: Exercise[]) => {
+        this.finishedExercisesChanged.next(exercises);
+      });
   }
 
+  private addDataToDatabase(exercise: Exercise) {
+    this.db.collection('finishedExercises').add(exercise);
+  }
 }
